80f02e82e98f5c585059a2b6db0905da8376cee3
Peter Marschall <PDMarschall@hotmail.com>
2022-09-29 10:41:17 +0200

    interface

--- AsyncFitness.Core/Interfaces/IRepository.cs
+++ AsyncFitness.Core/Interfaces/IRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AsyncFitness.Core.Interfaces
 {
-    internal interface IRepository
+    public interface IRepository<T>
     {
+        T ReturnEntity(string id);
+        IEnumerable<T> Select(Expression<Func<T, bool>> predicate);
+        T Update(T entity);
+        T Insert(T entity);
+        T Delete(T entity);
+        void SaveChanges();
     }
 }
