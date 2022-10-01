a6dcab0c78b70724af07ea12dd6cb2039a20d015
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-10-01 20:28:48 +0200

    landing page

--- AsyncFitness.Web/Controllers/EntryController.cs
+++ AsyncFitness.Web/Controllers/EntryController.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AsyncFitness.Core.Interfaces;
+using AsyncFitness.Core.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AsyncFitness.Web.Controllers
 {
     public class EntryController : Controller
     {
         private readonly ILogger<EntryController> _logger;
+        private readonly IRepository<GymCustomer> _repository;
 
-        public EntryController(ILogger<EntryController> logger)
+        public EntryController(ILogger<EntryController> logger, IRepository<GymCustomer> repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         public IActionResult Index()
@@ -16,14 +20,39 @@ namespace AsyncFitness.Web.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(GymCustomer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _repository.Add(customer);
+                    _repository.SaveChanges();
+                    return View("LandingPage", customer);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return View();
+            
+        }
+
         public IActionResult Login()
         {
-            return View();
+            var lars = _repository.Find(c => c.Email == "test@testmail.com");
+            var larsobjekt = lars.First();
+            var test = _repository.Count;
+            return View("LandingPage", larsobjekt);
         }
     }
 }
