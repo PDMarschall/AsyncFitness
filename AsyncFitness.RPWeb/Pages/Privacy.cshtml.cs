629fabe2a087a749e38b34d3611c5568a9cd66c0
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-10-13 12:54:24 +0200

    test getting user

--- AsyncFitness.RPWeb/Pages/Privacy.cshtml.cs
+++ AsyncFitness.RPWeb/Pages/Privacy.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace AsyncFitness.RPWeb.Pages
 {
@@ -8,14 +10,17 @@ namespace AsyncFitness.RPWeb.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        private readonly UserManager<IdentityUser> _manager;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        public PrivacyModel(ILogger<PrivacyModel> logger, UserManager<IdentityUser> manager)
         {
             _logger = logger;
+            _manager = manager;
         }
 
         public void OnGet()
         {
+            ViewData["UserEmail"] = User.FindFirstValue(ClaimTypes.Name);
         }
     }
 }
