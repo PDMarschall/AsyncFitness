9a6e3ce612a4252cf9e448aa951af3d7a52a498e
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-10-15 18:43:19 +0200

    test

---
+++ AsyncFitness.Web/Areas/Identity/Data/AsyncFitnessWebContext.cs
@@ -0,0 +1,22 @@
+﻿using AsyncFitness.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncFitness.Web.Data;
+
+public class AsyncFitnessWebContext : IdentityDbContext<AsyncFitnessUser>
+{
+    public AsyncFitnessWebContext(DbContextOptions<AsyncFitnessWebContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- AsyncFitness.Web/Program.cs
+++ AsyncFitness.Web/Program.cs
@@ -1,49 +1,47 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AsyncFitness.Web.Data;
+using AsyncFitness.Web.Areas.Identity.Data;
 
-namespace AsyncFitness.Web;
-
-public class Program
+namespace AsyncFitness.Web
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("AsyncFitnessWebContextConnection") ?? throw new InvalidOperationException("Connection string 'AsyncFitnessWebContextConnection' not found.");
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<AsyncFitnessWebContext>(options =>
             options.UseSqlServer(connectionString));
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddRazorPages();
+            builder.Services.AddDefaultIdentity<AsyncFitnessUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<AsyncFitnessWebContext>();
 
-        var app = builder.Build();
+            // Add services to the container.
+            builder.Services.AddRazorPages();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication(); ;
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
         }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapRazorPages();
-
-        app.Run();
     }
 }
