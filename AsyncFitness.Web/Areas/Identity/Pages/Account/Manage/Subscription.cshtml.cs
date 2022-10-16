093b60fb5f34f62f6991c2bdca9ac5b69f8f6512
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-10-16 11:27:44 +0200

    subscription page

---
+++ AsyncFitness.Web/Areas/Identity/Pages/Account/Manage/Subscription.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AsyncFitness.Web.Areas.Identity.Pages.Account.Manage
+{
+    public class SubscriptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
