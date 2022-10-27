cc2df8859e64a073f6a69dfb935ec05a984c82f3
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-10-27 13:40:26 +0200

    Revert "seed"
    This reverts commit b80ec25a535ecaee2e2612705afeee6369e5e579.

--- AsyncFitness.Web/SeedData.cs
+++ AsyncFitness.Web/SeedData.cs
@@ -240,7 +240,8 @@ namespace AsyncFitness.Web
                         Start = new DateTime(2022, 10, 4, 20, 0, 0),
                         End = new DateTime(2022, 10, 4, 21, 0, 0)
                     }
-                );
+                ); ;
+
 
                 context.SaveChanges();
             }
@@ -277,6 +278,7 @@ namespace AsyncFitness.Web
                     fitnessCenter.AvailableConcepts.Add(context.FitnessConcept.Find(2));       
                     context.SaveChanges();
                 }
+
             }
         }
     }
